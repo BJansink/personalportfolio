e8660c5a1bd2b1e5313b008b21148283046d0b1f
Barbara Jansink <barbarajansink@gmail.com>
2022-04-15 18:14:30 +0200

    Make all widths relative, fix skill details not overlaying each other

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // When the user clicks on <skill>, open the popup
 function myFunction(event) {
     console.log(event.target.id);
+   var parent = document.getElementById("skill-explanation");
+
+   var children = parent.children;
+   for (var i = 0; i < children.length; i++) {
+     var tableChild = children[i];
+     tableChild.classList.remove("show");
+   }
   var popup = document.getElementById(event.target.id+"-skill");
   popup.classList.toggle("show");
   console.log("HI all!!")
